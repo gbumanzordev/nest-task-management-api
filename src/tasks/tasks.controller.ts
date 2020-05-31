a874561389a2f7dfacfc1704ffa6b6089d0fd30c
GersonUmanzor <gbumanzor@outlook.com>
2020-05-31 16:32:21 -0600

    :sparkles: Finish video 22.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,20 +3,22 @@ import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { RequestResponse } from '../core/interfaces/response.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { PagedResponse } from '../core/interfaces/paged-response.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): RequestResponse<Task[]> {
+  getAllTasks(): PagedResponse<Task[]> {
     const data = this.tasksService.getAllTasks();
     return { data };
   }
 
   @Get('/:id')
-  getTaskById(@Param() id: string) {
-    return this.tasksService.getTaskById(id);
+  getTaskById(@Param('id') id: string): RequestResponse<Task> {
+    const data = this.tasksService.getTaskById(id);
+    return { data };
   }
 
   @Post()
